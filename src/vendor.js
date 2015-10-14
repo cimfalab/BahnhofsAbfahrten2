785a77811faa9a48291b134ce2e6233cada218cc
I <marudor@marudor.de>
2015-10-14 21:24:31 +0200

    remove unneccesary import

--- src/vendor.js
+++ src/vendor.js
@@ -3,6 +3,5 @@ global.When = require('when');
 global.Promise = global.When.Promise;
 global.axios = require('axios');
 global._ = require('lodash');
-global.classNames = require('classnames');
 import 'react-select/dist/default.css';
 import 'bootstrap-material-icons/css/material-icons.min.css';
